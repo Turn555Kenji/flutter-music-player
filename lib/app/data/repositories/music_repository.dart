d6a312b34b639d55b780fc800584437c0a06f427
Kenji555 <137581963+Turn555Kenji@users.noreply.github.com>
2026-04-25 22:24:45 -0300

    feat(repository): add playlist features

--- lib/app/data/repositories/music_repository.dart
+++ lib/app/data/repositories/music_repository.dart
@@ -4,6 +4,8 @@ import 'package:music_player/app/data/models/music.dart';
 import 'package:music_player/app/data/models/album.dart';
 import 'package:music_player/app/data/models/playlist.dart';
 
+int i = 1; //Replace later for real IDs
+
 class ProductsRepository {
   final List<Music> _musics = [];
   final List<Album> _albums = [];
@@ -28,5 +30,57 @@ class ProductsRepository {
     return playlists;
   }
 
-  
+  void createPlaylist(String name) {
+    _playlists.add(
+      Playlist(
+        id: i,
+        name: name,
+        coverUrl: "",
+        musicList: [],
+      ),
+    );
+    i=i+1;
+  }
+
+  ///////////////////////////////////////////////////////
+  Future<void> addProduct(String productName) async {
+    await Future.delayed(Duration(seconds: 2));
+    _productsList.add(
+      Product(title: productName),
+    );
+  }
+
+  Future<List<Product>> loadProducts() async {
+    await Future.delayed(Duration(seconds: 2));
+    return products;
+  }
+
+  void toggleCartItem(Product product) {
+    int productIndex = _cartItems.indexWhere(
+      (item) => item.product.title == product.title,
+    );
+    if (productIndex >= 0) {
+      _cartItems.removeAt(productIndex);
+      return;
+    }
+    _cartItems.add(CartItem(product: product, amount: 1));
+  }
+
+  void _changeAmount(CartItem cartItem, int amount) {
+    final index = cartItems.indexWhere(
+      (item) => item.product.title == cartItem.product.title,
+    );
+    _cartItems[index] = CartItem(product: cartItem.product, amount: amount);
+  }
+
+  void increase(CartItem cartItem) {
+    final amount = cartItem.amount + 1;
+    _changeAmount(cartItem, amount);
+  }
+
+  void decrease(CartItem cartItem) {
+    final amount = (cartItem.amount == 0) ? 0 : cartItem.amount - 1;
+    _changeAmount(cartItem, amount);
+  }
+}
 }
